07b1cafd860d90951d568180df91989306262cab
deimantejagu <deimajagu@outlook.com>
2024-11-25 15:36:23 +0200

    SCRAPER IS WORKIIIING

--- WebScraper/spiders/ActData.py
+++ WebScraper/spiders/ActData.py
@@ -1,24 +1,54 @@
 import scrapy
 import re
 
-class ActdataSpider(scrapy.Spider):
-    name = "ActData"
+class ActData(scrapy.Spider):
+    name = "ActData"  
+    # allowed_urls = ['e-seimas.lrs.lt']  
+    # start_urls = ["https://e-seimas.lrs.lt/portal/documentSearch/lt"]
 
     def start_requests(self):
-        url = "https://www.lrs.lt/pls/inter/w5_show?p_k=2&p_r=4005"
-        yield scrapy.Request(url, self.parse)
+        url = "https://e-seimas.lrs.lt/portal/documentSearch/lt"
+        yield scrapy.Request(url, meta=dict(playwright=True, playwright_include_page=True), callback=self.go_to_acts)
+
+    async def go_to_acts(self, response):
+        page = response.meta["playwright_page"]
+        try:
+            await page.locator('#searchCompositeComponent\\:contentForm\\:searchParamPane\\:searchButtonTop').click()
+            await page.wait_for_selector('tbody#searchCompositeComponent\\:contentForm\\:resultsTable_data')
+
+            urls = await self.extract_data(page)
+            urls = [response.urljoin(url) for url in urls]
+            
+            print(f"urls: {urls}")
+        finally:
+            await page.close()
+
+        yield scrapy.Request(url=response.url, meta={'playwright': True, 'urls': urls}, callback=self.parse, dont_filter=True)
+
+    async def extract_data(self, page):
+        links = page.locator('xpath=//tbody[@id="searchCompositeComponent:contentForm:resultsTable_data"]/tr/td[4]/a')
+
+        # Extract all href attributes
+        urls = []
+        link_count = await links.count()
+        for i in range(link_count):
+            href = await links.nth(i).get_attribute('href')
+            if href:
+                urls.append(href)
+
+        # pasiimti sios dienos data, issiextractinti kiekvieno lino data, paziureti ar sutampa su siandienos, jei sutampa, ideti linka i masyva.
+
+        return urls
 
     def parse(self, response):
-        # Extract URLs from the page
-        urls = response.xpath("//tr/td[2]/a/@href").getall()
+        # Retrieve the passed URLs
+        urls = response.meta.get('urls', [])
 
-        valid_urls = [url for url in urls if url]
-
-        yield from response.follow_all(valid_urls[:5], meta={'playwright': True}, callback=self.parse_act)
+        # Follow the first 5 URLs
+        yield from response.follow_all(urls[:10], meta={'playwright': True}, callback=self.parse_act)
 
     def parse_act(self, response):
-        # Parsing redirected page
-
+        # Parsing act page
         # Extract date
         date = response.xpath("//tr[contains(@class, 'ui-widget-content') and contains(@class, 'ui-panelgrid-even')]//td[8]/text()").get()
 
