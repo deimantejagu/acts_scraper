92c39fe39e900dafb9f0b99cf611893b53046a0e
deimantejagu <deimajagu@outlook.com>
2024-12-05 10:58:24 +0200

    added acts to db

--- WebScraper/Database/AddDataToDB.py
+++ WebScraper/Database/AddDataToDB.py
@@ -2,38 +2,46 @@ from CreateDbConnection import get_connection
 
 connection = get_connection()
 
-def insert_into_Acts(connection):
-    cursor = connection.cursor()
-
-    file_path = '/mnt/c/Users/Testinis/Desktop/acts-scraper/downloads/0cf5a27edd_MSO2010_DOCX.docx'
-    with open(file_path, 'rb') as file:
-        blob_data = file.read()
-
-    # Prepare the SQL INSERT statement with placeholders
-    sql_insert = """
+def create_Acts_placeholder():
+    sql_insert_Act = """
         INSERT INTO Acts (url, date, title, document)
         VALUES (?, ?, ?, ?)
     """
 
-    # Execute the INSERT statement with actual data
-    cursor.execute(sql_insert, ('https://e-seimas.lrs.lt/portal/legalAct/lt/TAP/8005d610b0b511efa81e811ff75635ec?positionInSearchResults=39&searchModelUUID=31cfffaf-e21d-4042-b6d6-38f86e2a8661', '2024-12-04', 'Dėl meilės negavimo', blob_data))
+    return sql_insert_Act
+
+def create_RelatedDocuments_placeholder():
+    sql_insert_RelatedDocuments = """
+        INSERT INTO RelatedDocuments (url, date, title, document)
+        VALUES (?, ?, ?, ?)
+    """
+
+    return sql_insert_RelatedDocuments
+
+def insert_into_Acts(connection):
+    cursor = connection.cursor()
+
+    # Test data
+    file_path = '/mnt/c/Users/Testinis/Desktop/acts-scraper/downloads/0cf5a27edd_MSO2010_DOCX.docx'
+    with open(file_path, 'rb') as file:
+        blob_data = file.read()
+
+    cursor.execute(create_Acts_placeholder(), 
+    (
+        'https://e-seimas.lrs.lt/portal/legalAct/lt/TAP/8005d610b0b511efa81e811ff75635ec?positionInSearchResults=39&searchModelUUID=31cfffaf-e21d-4042-b6d6-38f86e2a8661', 
+        '2024-12-04', 'Dėl meilės negavimo', 
+        blob_data
+    ))
     connection.commit()
     cursor.close()
 
 def insert_into_RelatedDocuments(connection):
     cursor = connection.cursor()
 
-    cursor.execute("""
-    INSERT INTO RelatedDocuments (url, act_id) VALUES (?, ?)
-    """, (
+    cursor.execute(create_RelatedDocuments_placeholder(), 
+    (
         'https://e-seimas.lrs.lt/portal/legalAct/lt/TAK/391ca611b0b611efa81e811ff75635ec?positionInSearchResults=38&searchModelUUID=31cfffaf-e21d-4042-b6d6-38f86e2a8661',
-        1  
-    ))
-    cursor.execute("""
-    INSERT INTO RelatedDocuments (url, act_id) VALUES (?, ?)
-    """, (
-        'https://e-seimas.lrs.lt/portal/legalAct/lt/TAK/391ca611b0b611efa81e811ff75635ec?positionInSearchResults=38&searchModelUUID=31cfffaf-e21d-4042-b6d6-38f86e2a8661',
-        1  
+        1 
     ))
 
     connection.commit()
--- WebScraper/pipelines.py
+++ WebScraper/pipelines.py
@@ -6,19 +6,16 @@
 
 # useful for handling different item types with a single interface
 
-import hashlib
-
-from itemadapter import ItemAdapter
 from scrapy.pipelines.files import FilesPipeline
 
 class WebscraperPipeline:
-    def process_item(self, item, spider):
+    def process_item(self, item):
+
         return item
     
 class DocxFilePipeline(FilesPipeline):
-    def file_path(self, request, response=None, info=None, *, item=None):
-        file_url_hash = hashlib.shake_256(request.url.encode()).hexdigest(5)
-        file_perspective = request.url.split("/")[-2]
-        filename = f"{file_url_hash}_{file_perspective}.docx"
+    def file_path(self, request):
+        file_perspective = request.url.split("/")[-4]
+        filename = f"{file_perspective}.docx"
 
         return filename
