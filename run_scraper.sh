df419ace5757c9c7e4b24b7b294af8fd7c02e9a2
deimantejagu <deimajagu@outlook.com>
2025-01-14 08:51:05 +0200

    removed cashed file

--- run_scraper.sh
+++ run_scraper.sh
@@ -4,55 +4,6 @@
 LOCK_FILE="/tmp/acts_scraper.lock"
 docx_download="storage/docx_downloads"
 STORAGE_DIR="storage/"
-LOG_FILE="/path/to/logfile.log"
-SCRIPT_PATH="/mnt/c/Users/Testinis/Desktop/acts-scraper/run_scraper.sh"
-CRON_LOG="/mnt/c/Users/Testinis/Desktop/acts-scraper/cron_test.log"
-
-# Function to reset the script or perform any cleanup
-reset_script() {
-    echo "Resetting the script..."
-    # Stop the cron job
-    echo "Disabling cron job..."
-    crontab -l | grep -v "/path/to/your/script.sh" | crontab -
-
-        # Delete lock file if it exists
-    if [ -f "$LOCK_FILE" ]; then
-        echo "Deleting lock file..."
-        rm -f "$LOCK_FILE"
-    fi
-
-    # Delete storage directory or reset it
-    if [ -d "$STORAGE_DIR" ]; then
-        echo "Deleting storage directory..."
-        rm -rf "$STORAGE_DIR"  # Caution: This will delete the storage completely
-    fi
-
-    # Clear log file (optional)
-    if [ -f "$LOG_FILE" ]; then
-        echo "Clearing log file..."
-        > "$LOG_FILE"  # Clears the log file
-    fi
-
-    echo "Reset actions completed."
-
-    if [[ "$current_time" == "23:59" ]]; then
-        echo "It's 23:59, stopping and resetting the script..."
-        # Re-enable the cron job (add back to crontab)
-        echo "Re-enabling cron job..."
-
-        # Run the script immediately after reset
-        echo "Starting the script immediately..."
-        /bin/bash /mnt/c/Users/Testinis/Desktop/acts-scraper/run_scraper.sh
-    fi
-}
-
-current_time=$(date +"%H:%M")
-
-if [[ "$current_time" == "23:55" ]]; then
-    echo "It's 23:55, stopping and resetting the script..."
-    reset_script
-    exit 0
-fi
 
 # Check if the lock file exists. If it does, exit the script.
 if [ -e $LOCK_FILE ]; then
@@ -70,7 +21,7 @@ cd /mnt/c/Users/Testinis/Desktop/acts-scraper || exit && \
 # Scrape acts and add to .json
 scrapy crawl ActData -o storage/output.json:json && \
 
-# Create databaseif not exists
+# Create database if not exists
 DATABASE="database/ActsData.db"
 if [ ! -f "$DATABASE" ]; then
     python3 -m database.create_db
